e8575e624dafb4715d05ece7010037ceb6704c5f
lkenigsb <libbykenigsberg@gmail.com>
2023-02-09 10:41:02 -0500

    MorseCode update array initialization & return StringBuilder

--- src/kenigsberg/MorseCode/MorseCode.java
+++ src/kenigsberg/MorseCode/MorseCode.java
@@ -9,64 +9,13 @@ public class MorseCode
     public MorseCode()
     {
 
-        this.morseCode = new String[26]; //every object created, creates an array of morse code values each index matching to letter in alphabet
+        this.morseCode = new String[] {"* ---","--- * * *","--- * --- *","--- * *","*","* * --- *","--- --- *","* * * *","* *",
+                "* --- --- ---","--- * ---","* --- * *","--- ---","--- *","--- --- ---","* --- --- *","--- --- * ---",
+                "* --- *","* * *","---","* * ---","* * * ---","* --- ---","--- * * ---","--- * --- ---","--- --- * *"};
 
-        morseCode[0] = "* ---";//A
-        morseCode[1] = "--- * * *";//B
-        morseCode[2] = "--- * --- *";//C
-        morseCode[3] = "--- * *";//D
-        morseCode[4] = "*";//E
-        morseCode[5] = "* * --- *";//F
-        morseCode[6] = "--- --- *";//G
-        morseCode[7] = "* * * *";//H
-        morseCode[8] = "* *";//I
-        morseCode[9] = "* --- --- ---";//J
-        morseCode[10] = "--- * ---";//K
-        morseCode[11] = "* --- * *";//L
-        morseCode[12] = "--- ---";//M
-        morseCode[13] = "--- *";//N
-        morseCode[14] = "--- --- ---";//O
-        morseCode[15] = "* --- --- *";//P
-        morseCode[16] = "--- --- * ---";//Q
-        morseCode[17] = "* --- *";//R
-        morseCode[18] = "* * *";//S
-        morseCode[19] = "---";//T
-        morseCode[20] = "* * ---";//U
-        morseCode[21] = "* * * ---";//V
-        morseCode[22] = "* --- ---";//W
-        morseCode[23] = "--- * * ---";//X
-        morseCode[24] = "--- * --- ---";//Y
-        morseCode[25] = "--- --- * *";//Z
 
-        this.letters = new String[26];
-
-        letters[0] = "A";
-        letters[1] = "B";
-        letters[2] = "C";
-        letters[3] = "D";
-        letters[4] = "E";
-        letters[5] = "F";
-        letters[6] = "G";
-        letters[7] = "H";
-        letters[8] = "I";
-        letters[9] = "J";
-        letters[10] = "K";
-        letters[11] = "L";
-        letters[12] = "M";
-        letters[13] = "N";
-        letters[14] = "O";
-        letters[15] = "P";
-        letters[16] = "Q";
-        letters[17] = "R";
-        letters[18] = "S";
-        letters[19] = "T";
-        letters[20] = "U";
-        letters[21] = "V";
-        letters[22] = "W";
-        letters[23] = "X";
-        letters[24] = "Y";
-        letters[25] = "Z";
-
+        this.letters = new String[] {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U",
+        "V","W","X","Y","Z"};
     }
 
 
@@ -95,8 +44,6 @@ public class MorseCode
             }
 
         }
-
-
         return morseCode.toString();//message in morse code using - & *
     }
 
@@ -124,6 +71,6 @@ public class MorseCode
         }
         
 
-        return String.valueOf(message);
+        return message.toString();
     }
 }
